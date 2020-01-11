a5ab398c1d300a50b2f2a455c067418f8f31527d
Julian Szachowicz <295013@student.mini.pw.edu.pl>
2020-01-11 11:55:47 +0100

    Testing GCF

--- functions/main.py
+++ functions/main.py
@@ -1,9 +1,22 @@
+import sys
+
+from google.cloud import storage
+
+from google.appengine.api import images
+
+
 def gcf1_rescale(event, context):
+
+    #   storage_client = storage.Client()
+    #   target_bucket = storage_client.bucket('project-ii-gae-bucket-2')
+
+
     """Triggered by a change to a Cloud Storage bucket.
     Args:
          event (dict): Event payload.
          context (google.cloud.functions.Context): Metadata for the event.
     """
+
     file = event
-    print(f"Processing file: {file['name']}.")
+    print(f"Processing file:\n\n{event}\n\n{context}.")
 
