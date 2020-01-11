1f8d118d059533101fe3228a12740f4140c71186
Julian Szachowicz <295013@student.mini.pw.edu.pl>
2020-01-11 10:50:43 +0100

    Prepare repo for first gcf

---
+++ functions/gcf1_rescale.py
@@ -0,0 +1,9 @@
+def hello_gcs(event, context):
+    """Triggered by a change to a Cloud Storage bucket.
+    Args:
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
+    """
+    file = event
+    print(f"Processing file: {file['name']}.")
+
