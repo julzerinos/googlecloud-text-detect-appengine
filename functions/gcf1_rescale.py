a1297c4123a2fc767d560242980f0fc8cdcd85c4
Julian Szachowicz <295013@student.mini.pw.edu.pl>
2020-01-11 11:15:53 +0100

    Remove clutter

--- functions/gcf1_rescale.py
+++
@@ -1,9 +0,0 @@
-def gcf1_rescale(event, context):
-    """Triggered by a change to a Cloud Storage bucket.
-    Args:
-         event (dict): Event payload.
-         context (google.cloud.functions.Context): Metadata for the event.
-    """
-    file = event
-    print(f"Processing file: {file['name']}.")
-
