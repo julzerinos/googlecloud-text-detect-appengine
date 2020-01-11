a39d0e62918e83a69a43bde8227905ed074cc76f
Julian Szachowicz <295013@student.mini.pw.edu.pl>
2020-01-11 12:54:59 +0100

    GCF testing cont.

--- functions/main.py
+++ functions/main.py
@@ -8,7 +8,7 @@ def gcf1_rescale(event, context):
     storage_client = storage.Client()
     source_bucket = storage_client.bucket('project-ii-gae-bucket-1')
 
-    blob = bucket.blob(event['name'])
+    blob = source_bucket.blob(event['name'])
 
     file = event
     print(f"BLOB ==== {blob}.")
