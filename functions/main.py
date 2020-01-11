c8356971e73a8ef8936c92bdb49f0d7f09a0f527
Julian Szachowicz <295013@student.mini.pw.edu.pl>
2020-01-11 13:37:23 +0100

    GCF testing cont.

--- functions/main.py
+++ functions/main.py
@@ -1,9 +1,16 @@
-import sys
+# sources
+#   google storage blob python ref
+#   https://googleapis.dev/python/storage/latest/blobs.html
+#
+#   image manipulations with PIL and google storage
+#   https://stackoverflow.com/questions/55941068/change-image-size-with-pil-in-a-google-cloud-storage-bucket-from-a-vm-in-gcloud
+
 import io
 
 from google.cloud import storage
 
 from PIL import Image
+
 
 def gcf1_rescale(event, context):
 
@@ -12,11 +19,13 @@ def gcf1_rescale(event, context):
 
     blob = source_bucket.blob(event['name']).download_as_string()
     im = Image.open(io.BytesIO(blob))
-    im.thumbnail(512, Image.ANTIALIAS)
+    x, y = im.size
 
-    target_bucket = storage_client.bucket('project-ii-gae-bucket-2')
-    new_blob = target_bucket.blob(event['name'])
-    new_blob.upload_from_string(im.tobytes())    
+    # im.thumbnail(512, Image.ANTIALIAS)
 
-    print(f"Successfully resized and saved to bucket-2")
+    # target_bucket = storage_client.bucket('project-ii-gae-bucket-2')
+    # new_blob = target_bucket.blob(event['name'])
+    # new_blob.upload_from_string(im.tobytes())
 
+    print(x, y)
+    # print("Successfully resized and saved to bucket-2")
