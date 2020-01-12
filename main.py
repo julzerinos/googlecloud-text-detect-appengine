1e1079a949bfa3066c05630f63219128465cd9ed
Julian Szachowicz <295013@student.mini.pw.edu.pl>
2020-01-12 11:00:29 +0100

    Frontend changes

--- main.py
+++ main.py
@@ -1,27 +1,3 @@
-# sources - flask & google
-#   relative paths in app engine
-#   https://stackoverflow.com/questions/61894/whats-a-good-way-to-find-relative-paths-in-google-app-engine
-#
-#   read Flask uploads from memory
-#   https://stackoverflow.com/questions/20015550/read-file-data-without-saving-it-in-flask
-#
-#   templates in App Engine Flask
-#   https://books.google.pl/books?id=9QdYgH5mEi8C&pg=PA120&lpg=PA120&dq=app+engine+templates+folder&source=bl&ots=bbroQ5Sgoo&sig=ACfU3U1VFKmcUTDkCOP9KgUImkfRhuRyGQ&hl=en&sa=X&ved=2ahUKEwjDrLbT5vvmAhWpl4sKHRiZC0QQ6AEwAnoECAoQAQ#v=onepage&q=app%20engine%20templates%20folder&f=false
-#
-# sources - google auth & users
-#   google sign-in
-#   https://developers.google.com/identity/sign-in/web
-#
-# sources - images & hash
-#   hashing
-#   https://ourcodeworld.com/articles/read/1006/how-to-determine-whether-2-images-are-equal-or-not-with-the-perceptual-hash-in-python
-#
-#   reading Flask FileStorage into Pillow
-#   https://stackoverflow.com/questions/17733133/loading-image-from-flasks-request-files-attribute-into-pil-image
-# sources - datastore
-#   datastore python ref
-#   https://googleapis.dev/python/datastore/latest/index.html
-
 import time
 
 from google.cloud import storage
@@ -44,7 +20,7 @@ def index():
         f = request.files['file']
         if f:
 
-            digital_digest = imagehash.phash(
+            digital_digest = imagehash.average_hash(
                 Image.open(f.stream)
                 )
 
@@ -99,3 +75,32 @@ def index():
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
+
+
+# sources - flask & google
+#   relative paths in app engine
+#   https://stackoverflow.com/questions/61894/whats-a-good-way-to-find-relative-paths-in-google-app-engine
+#
+#   read Flask uploads from memory
+#   https://stackoverflow.com/questions/20015550/read-file-data-without-saving-it-in-flask
+#
+#   templates in App Engine Flask
+#   https://books.google.pl/books?id=9QdYgH5mEi8C&pg=PA120&lpg=PA120&dq=app+engine+templates+folder&source=bl&ots=bbroQ5Sgoo&sig=ACfU3U1VFKmcUTDkCOP9KgUImkfRhuRyGQ&hl=en&sa=X&ved=2ahUKEwjDrLbT5vvmAhWpl4sKHRiZC0QQ6AEwAnoECAoQAQ#v=onepage&q=app%20engine%20templates%20folder&f=false
+#
+#   uploading files
+#   https://flask.palletsprojects.com/en/1.1.x/patterns/fileuploads/
+#
+# sources - google auth & users
+#   google sign-in
+#   https://developers.google.com/identity/sign-in/web
+#
+# sources - images & hash
+#   hashing
+#   https://ourcodeworld.com/articles/read/1006/how-to-determine-whether-2-images-are-equal-or-not-with-the-perceptual-hash-in-python
+#
+#   reading Flask FileStorage into Pillow
+#   https://stackoverflow.com/questions/17733133/loading-image-from-flasks-request-files-attribute-into-pil-image
+#
+# sources - datastore
+#   datastore python ref
+#   https://googleapis.dev/python/datastore/latest/index.html
